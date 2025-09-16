b91d462f2c9cf9b23f33e5c793e98ae6ff75828f
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-09-16 15:31:29 +0500

    commit13

--- src/components/Home/Mirror.jsx
+++ src/components/Home/Mirror.jsx
@@ -39,7 +39,7 @@ const Mirror = () => {
   }, [])
 
   return (
-    <div className='w-full h-[300rem] bg-secondary flex flex-col justify-start items-center relative top-56'>
+    <div className='w-full h-[190rem] bg-secondary flex flex-col justify-start items-center relative top-56'>
       {/* Fade on first image */}
       <motion.img
         src="/Mirror1.png"
@@ -93,6 +93,30 @@ const Mirror = () => {
           Your key to <br /> Exceptional Success
         </motion.h1>
       </motion.div>
+      <div className='w-full h-auto flex relative z-20 flex-col justify-center items-start'>
+        <img src="/Mirror2.svg" alt="Mirror2" className='w-full h-auto object-contain absolute z-10 top-[-20rem]' />
+        <div className='absolute z-30 w-full h-[30rem] top-[5rem] flex flex-col justify-around items-start'>
+          <div className='w-[40%] h-[10rem] absolute z-30 left-64 top-10 flex flex-col justify-around items-start'>
+            <h1 className='text-7xl text-primary font-bold'>We've all been there</h1>
+            <p className='text-2xl text-gray-500 font-medium'>You're away from home and unsure <br /> where you can <span className='text-black font-medium'>go</span>. </p>
+          </div>
+          <img src="/Home-character2.svg" alt="char2" className='absolute z-30 left-[70rem] top-[1rem] h-[30rem] w-[20rem]' />
+        </div>
+        <div className='absolute z-30 w-full h-[40rem] top-[45rem] flex flex-col justify-around items-start'>
+          <img src="/Home-character3.svg" alt="char3" className='absolute top-[8rem] left-[10rem] h-[30rem] w-[20rem]' />
+          <div className='w-[40%] h-[25rem] absolute z-30 left-[60rem] top-[1rem] flex flex-col justify-around items-start'>
+            <h1 className='text-7xl text-primary font-bold'>A tiny ask shouldn’t <br /> feel so big.</h1>
+            <p className='text-2xl text-gray-500 font-medium'>Waiting in line, holding it in, just to <span className='text-black font-medium'>beg</span> <br /> for a restroom key. It’s awkward. I don’t <br /> want to tell someone I have to evacuate <br /> my bowels.</p>
+          </div>
+        </div>
+        <div className='absolute z-30 w-full h-[50rem] top-[90rem] flex flex-col justify-around items-start'>
+          <div className='w-[40%] h-[20rem] absolute z-30 left-64 top-10 flex flex-col justify-around items-start'>
+            <h1 className='text-7xl text-primary font-bold'>No more asking permission</h1>
+            <p className='text-2xl text-gray-500 font-medium'>With Yedi, it’s easy go straight in and <br /> find the bathroom door, scan and <br /> unlock to find <span className='text-black font-medium'>relief</span>. </p>
+          </div>
+          <img src="/Home-character4.svg" alt="char4" className='absolute z-30 left-[70rem] top-[13rem] h-[30rem] w-[20rem]' />
+        </div>
+      </div>
     </div>
   )
 }
